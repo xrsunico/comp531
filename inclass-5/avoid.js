903cec2b80f5490ddf67edb44118aa9cff8fb053
xrsunico <xrsunico@gmail.com>
2017-01-24 21:38:01 -0600

    Add files via upload

---
+++ inclass-5/avoid.js
@@ -0,0 +1,37 @@
+window.onload=function(){
+    var start=document.getElementById("click");
+    var clicked=false;
+    document.getElementById("box").style.display='none';
+    var w = window.innerWidth-start.offsetWidth; 
+    var h = window.innerHeight-start.offsetHeight;
+    start.style.position="absolute";
+    start.addEventListener("mouseover",avoid,true);
+    start.addEventListener("click",start.onclick,false);
+
+    start.onclick=function click(){
+        if(!clicked){
+            clicked=true;
+            start.value="Play Again";
+            //start.removeEventListener("end",avoid);
+            document.getElementById("box").style.display='';
+        }
+        else{
+            document.getElementById("box").style.display='none';
+            clicked=false;
+            start.value="Click Me";
+            avoid();
+        }
+    }
+    function avoid(){
+        if (event.shiftKey){	
+            return;
+        }else if(start.value =="Play Again"){
+                return;
+        }else{					
+            var randomW=Math.floor(Math.random()*w);
+            var randomH=Math.floor(Math.random()*h);
+            start.style.left=randomW+"px";
+            start.style.top=randomH+"px";
+        }
+    }
+}
