f4482f7b8150e22f34d72f81412ec2821d3376ad
xrsunico <xrsunico@gmail.com>
2017-02-02 23:09:26 -0600

    Create particle.js

---
+++ inclass-8/particle.js
@@ -0,0 +1,28 @@
+const random = (min=0, max=800) =>
+    Math.random() * (max - min) + min
+
+// default values
+const particle = ({
+    mass=random(5, 30),
+    position=[random(), random()],
+    velocity=[random(-0.1, 0.1), random(-0.1, 0.1)],
+    acceleration=[0, 0]
+} = {}) => {
+    return {acceleration, velocity, position, mass}
+}
+
+const update = ({acceleration, velocity, position, mass}, delta, canvas) => {
+	velocity[0] = velocity[0] + acceleration[0]*delta;
+    velocity[1] = velocity[1] + acceleration[1]*delta;
+    position[0] = position[0]+ velocity[0] * delta;
+    position[1] = position[0]+ velocity[1] * delta;
+    if(position[0]<0) position[0] = position[0] + canvas.width;
+    if(position[0]>canvas.width) position[0] = position[0]-canvas.width;
+    if(position[1]<0) position[1] = position[1]+canvas.width;
+    if(position[1]>canvas.width) position[1] = position[1]-canvas.width;
+    return { mass, acceleration, velocity, position }
+}
+
+export default particle
+
+export { update }
